06d23632f1832310f5ab0186978cd2369a335567
jiayuez <Jiayue.Zheng@metropolia.fi>
2024-08-28 10:36:51 +0300

    Complete FE Pair Programming 1 Part 1

---
+++ src/IdCard.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './IdCard.css'; 
+
+function IdCard(props) {
+    return (
+        <div className="id-card">
+            <img className="id-card-picture" src={props.picture} alt="Profile" />
+            <div className="id-card-info">
+                <ul>
+                    <li><b>First Name:</b> {props.firstName}</li>
+                    <li><b>Last Name:</b> {props.lastName}</li>
+                    <li><b>Gender:</b> {props.gender}</li>
+                    <li><b>Height:</b> {props.height} cm</li>
+                    <li><b>Birth:</b> {props.birth.toDateString()}</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default IdCard;
